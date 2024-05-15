cb559270b1c37cdca17d253a72b1fa82a25495ba
danielntow <danielntow@gmail.com>
2024-05-15 13:37:50 +0000

    advanced mysql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Script to create the 'users' table with the specified attributes
+
+-- Ensure that the table doesn't already exist
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
