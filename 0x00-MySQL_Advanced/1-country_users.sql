5e548abdd4df30f03701799fdeb0dbdb5a17821a
danielntow <danielntow@gmail.com>
2024-05-15 13:57:43 +0000

    advanced mysql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Script to create the 'users' table with the specified attributes
+
+-- Ensure that the table doesn't already exist
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
