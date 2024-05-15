012b9576cafd922cd5c9c0722f1a3a14d31057ba
danielntow <danielntow@gmail.com>
2024-05-15 14:36:59 +0000

    advanced mysql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,22 +1,16 @@
--- create a stored procedure that adds a new correction for a student
-DELIMITER |
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+-- SQL script that creates a stored
+-- procedure AddBonus that adds a new correction for a student.
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INTEGER, IN project_name VARCHAR(255), IN score INTEGER)
 BEGIN
--- check if projects table has a name column
-IF COL_LENGTH('projects', 'name') IS NULL
-THEN
--- if if doesn't exist, put it there
-ALTER TABLE projects
-ADD COLUMN name VARCHAR(255);
--- insert the new project details
-INSERT INTO projects (name) VALUES (project_name);
-END IF;
--- get the id of the project passed
-SET @x := (SELECT id FROM projects WHERE name = project_name);
+    INSERT INTO projects(name)
+    SELECT project_name FROM DUAL
+    WHERE NOT EXISTS(SELECT * FROM projects WHERE name = project_name LIMIT 1);
 
--- insert the userid, project id and score into the corrections table
-INSERT INTO corrrections (user_id, project_id, score)
-VALUES (user_id, @pro_id, score);
-END;
-|
-DELIMITER ;
+    INSERT INTO corrections(user_id, project_id, score)
+    VALUES (
+    user_id,
+    (SELECT id FROM projects WHERE name = project_name),
+    score);
+END $$
+DELIMITER;
